bbb0583cf089aeaa4ca66b3bd6b40e15c5a180a7
Tommy Connor <tconnor98@aol.com>
2019-12-02 20:25:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  i = 0 
+  while i < dwarves.length 
+  puts "#{i + 1}. #{dwarves[i]}"
+  i += 1 
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  
 end
 
 def long_planeteer_calls# code an argument here
