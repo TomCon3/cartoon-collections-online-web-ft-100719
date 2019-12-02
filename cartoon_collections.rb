e11929bb538644117b34cf9a1fe1b336880b4d84
Tommy Connor <tconnor98@aol.com>
2019-12-02 21:04:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,9 @@ def long_planeteer_calls(long_calls)
   i = 0 
   if long_calls.any?{[i] i.length > 4}
     return TRUE
+  else
+    return FALSE
+    
 end
 
 def find_the_cheese# code an argument here
